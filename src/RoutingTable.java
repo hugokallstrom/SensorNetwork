fc3034be93ee6433e877342d9d69a586225c3010
ViktorLindblad <viktor.lindblad123@gmail.com>
2015-05-11 15:33:30 +0200

    added routingtable

--- src/RoutingTable.java
+++ src/RoutingTable.java
@@ -1,6 +1,35 @@
 /**
  * Created by hugo on 5/11/15.
  */
+import java.util.*;
 public class RoutingTable {
-	private 
+	private ArrayList<Event> events;
+	
+	public RoutingTable(){
+		events = new ArrayList<Event>();
+	}
+	public void syncEvents(ArrayList<Event> e){
+		/*Denna metod är inte färdig*/
+		for(Event event : e ){
+			if(!events.contains(event)){
+				events.add(event);
+			}
+		}
+		for(Event event : events){
+			if(!e.contains(event)){
+				e.add(event);
+			}
+		}
+			
+	}
+	public void addEvent(Event e){
+		events.add(e);
+	}
+	public Event getEvent(Event e){
+		if(events.contains(e)){
+				return e;
+		}
+		throw new IllegalArgumentException("Event's id was not found! in "
+				+ " routingTable");
+	}
 }
