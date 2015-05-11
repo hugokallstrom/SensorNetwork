161be910283b9c5e50eaebf32b03ff9a065b6773
ViktorLindblad <viktor.lindblad123@gmail.com>
2015-05-11 13:28:21 +0200

    added position

--- src/Position.java
+++ src/Position.java
@@ -2,4 +2,17 @@
  * Created by hugo on 5/11/15.
  */
 public class Position {
+	private int X;
+	private int Y;
+	
+	public Position(int x, int y){
+		X=x;
+		Y=y;
+	}
+	public int getX(){
+		return X;
+	}
+	public int getY(){
+		return Y;
+	}
 }
