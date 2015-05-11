2ab4e1fa6d3e7d3d9cc04bb0fae2d1a5bc75d47c
ViktorLindblad <viktor.lindblad123@gmail.com>
2015-05-11 13:47:23 +0200

    added query

--- src/Query.java
+++ src/Query.java
@@ -1,5 +1,21 @@
 /**
  * Created by hugo on 5/11/15.
  */
+import java.util.*;
+
 public class Query {
+	private Stack<Node> pathTaken;
+	private Hashtable<Position, Node> visited;
+	private int steps;
+	private Event event;
+	private int timeToLive;
+
+	public Query(Event e){
+		
+		timeToLive=0;
+		steps=0;
+		visited=new Hashtable<Position,Node>();
+		pathTaken = new Stack<Node>();
+		event=e;
+	}
 }
