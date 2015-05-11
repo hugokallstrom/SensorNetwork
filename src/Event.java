9bb8fe3a53ef3e679d234e1ba7d972c8dddf0467
charlotteristiniemi <charlotteristiniemi@client217-148.wireless.umu.se>
2015-05-11 14:34:38 +0200

    test

--- src/Event.java
+++ src/Event.java
@@ -2,4 +2,40 @@
  * Created by hugo on 5/11/15.
  */
 public class Event {
+	
+	private int eventId;
+	private int distance;
+	private Position position;
+	private int timeOfEvent;
+	
+	
+	public Event(int eventId){
+		this.eventId = eventId;
+		distance = 0;
+		
+	}
+	
+	public int getEventId(){
+		return eventId;
+	}
+	
+	public int getDistance(){
+		return distance;
+	}
+	
+	public void setDistance(int distance){
+		this.distance = distance;
+	}
+	
+	public Position getPosition(){
+		return position;
+	}
+	
+	public int getTimeOfEvent(){
+		return timeOfEvent;
+	}
+	
+	public void setTimeOfEvent(int timeOfEvent){
+	this.timeOfEvent = timeOfEvent;	
+	}
 }
